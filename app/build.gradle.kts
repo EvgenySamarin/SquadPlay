3c89f0991932b7afa7ddb4680eaf51d4ff6e7e60
evgeny.samarin <evgeny.samarin@lenta.com>
2026-07-05 12:10:10 +0200

    update code version
    Signed-off-by: evgeny.samarin <evgeny.samarin@lenta.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.eysamarin.squadplay"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
+        versionCode = 13
         versionName = "0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
