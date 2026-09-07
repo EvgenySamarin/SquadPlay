dc6840488cc7e01ddf250ddec02dd39bbe868946
evgeny.samarin <evgeny.samarin@lenta.com>
2026-09-07 12:04:19 +0200

    chore: bump versionCode to 16 and versionName to 0.8.3
    Signed-off-by: evgeny.samarin <evgeny.samarin@lenta.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.eysamarin.squadplay"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 15
-        versionName = "0.8.2"
+        versionCode = 16
+        versionName = "0.8.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
