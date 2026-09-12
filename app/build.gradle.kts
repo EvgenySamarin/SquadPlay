5f8d4c83e0b611c0d547283773d6bbd366f365a6
EvgenySamarin <ey.samarin@gmail.com>
2026-09-12 02:05:55 +0200

    chore: bump versionCode to 23
    Signed-off-by: EvgenySamarin <ey.samarin@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.eysamarin.squadplay"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 22
-        versionName = "0.9.2"
+        versionCode = 23
+        versionName = "0.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
